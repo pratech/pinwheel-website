92256a462614c06cb6bd521d40466ff243bcc783
Prateek <prateek.ideafires@gmail.com>
2026-07-06 05:36:03 +0530

    feat(makers-club): add landing page, Google Form registration, and club information

--- js/config.js
+++ js/config.js
@@ -1,6 +1,8 @@
 const CONFIG = {
 youtubeVideoId: "PEYewoyIOyY",
 formspreeEndpoint: "https://formspree.io/f/xdaybqlg",
+makersClubRegistration: "https://forms.gle/wYa7HwiUFZzdC7yA9",
+makersClubVideoId: "InjkMCQqiAs",
 chatbotApi: "https://api.pinwheel.in/chat",
 sessionId: "user-1"
 };
